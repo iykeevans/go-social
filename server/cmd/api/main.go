dee5e5769e1fb2d9136de49fd25b64c5f1b40ed2
Ezeani Ikenna Evan <32799580+iykeevans@users.noreply.github.com>
2025-02-08 22:54:45 +0100

    chore: init project

---
+++ server/cmd/api/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+
+	"github.com/iykeevans/go-social/server/internal/env"
+)
+
+func main() {
+	cfg := config{
+		addr: env.GetString("ADDR", ":8080"),
+	}
+
+	app := &application{
+		config: cfg,
+	}
+
+	mux := app.mount()
+
+	log.Fatal(app.run(mux))
+}
---
+++ server/go.mod
@@ -0,0 +1,5 @@
+module github.com/iykeevans/go-social/server
+
+go 1.21.6
+
+require github.com/go-chi/chi/v5 v5.2.1 // indirect
